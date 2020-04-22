03b1b987ae1f8b07e9eab570c82a106c2bdbe875
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-22 21:33:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,6 +10,10 @@ end
 
 def countdown_with_sleep(count_num)
   while count_num > 0
-  puts "#{count_num} SECOND(S)!"
-  sleep(1)
-  
+    puts "#{count_num} SECOND(S)!"
+    sleep(1)
+    count_num -= 1
+  end  
+    "HAPPY NEW YEAR!"
+  end 
+      
